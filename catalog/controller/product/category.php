c82446a6b640c281ddfd066ac90f2cdc29cd7d32
Shawn Lin <jallen0927@hotmail.com>
2015-07-24 15:03:02 +1200

    Fix description html encode

--- catalog/controller/product/category.php
+++ catalog/controller/product/category.php
@@ -270,7 +270,7 @@ class ControllerProductCategory extends Controller {
 					'product_id'  => $result['product_id'],
 					'thumb'       => $image,
 					'name'        => $result['name'],
-					'description' => $this->state === 'sold' ? $result['description'] : utf8_substr(strip_tags(html_entity_decode($result['description'], ENT_QUOTES, 'UTF-8')), 0, 200) . '..',
+					'description' => $this->state === 'sold' ? strip_tags(html_entity_decode($result['description'], ENT_QUOTES, 'UTF-8')) : utf8_substr(strip_tags(html_entity_decode($result['description'], ENT_QUOTES, 'UTF-8')), 0, 200) . '..',
 					'price'       => $price,
 					'special'     => $special,
 					'tax'         => $tax,
